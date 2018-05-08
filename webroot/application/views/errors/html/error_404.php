90c9b440ceb6c6ba8e25f3f2916f7b1bfca6a086
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-05-08 15:29:05 -0500

    Actualización show_404()

--- webroot/application/views/errors/html/error_404.php
+++ webroot/application/views/errors/html/error_404.php
@@ -1,64 +1,63 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+?>
+<!DOCTYPE html>
+<html lang="es">
+
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
-
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="EV Development">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="http://evpiudvlp.local/assets/themes/elaadmin/images/favicon.png">
+    <title><?php echo $heading; ?></title>
+    <!-- Bootstrap Core CSS -->
+    <link href="http://evpiudvlp.local/assets/themes/elaadmin/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="http://evpiudvlp.local/assets/themes/elaadmin/css/helper.css" rel="stylesheet">
+    <link href="http://evpiudvlp.local/assets/themes/elaadmin/css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
+    <!--[if lt IE 9]>
+    <script src="https:**oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+
+<body class="fix-header fix-sidebar">
+    <!-- Preloader - style you can find in spinners.css -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>
+    <!-- Main wrapper  -->
+    <div class="error-page" id="wrapper">
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1>404</h1>
+                <h3 class="text-uppercase"><?php echo $heading; ?> </h3>
+                <p class="text-muted m-t-30 m-b-30"><?php echo $message; ?></p>
+                <a class="btn btn-info btn-rounded waves-effect waves-light m-b-40" href="http://evpiudvlp.local/auth/index">Regresar al inicio</a> </div>
+            <footer class="footer text-center">&copy; <script>var d = new Date(); var n = d.getFullYear(); document.write(n);</script> CI Estrada Velasquez - Todos los derechos reservados.</footer>
+        </div>
+    </div>
+    <!-- End Wrapper -->
+    <!-- All Jquery -->
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/lib/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/lib/bootstrap/js/popper.min.js"></script>
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/jquery.slimscroll.js"></script>
+    <!--Menu sidebar -->
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="http://evpiudvlp.local/assets/themes/elaadmin/js/custom.min.js"></script>
+
 </body>
+
 </html>

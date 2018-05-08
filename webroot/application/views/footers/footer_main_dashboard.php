7b1be8083051e4450d173ce6f3ce27af6a8862b0
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-05-08 13:39:01 -0500

    Actualización de plantillas header y footer del dashboard

---
+++ webroot/application/views/footers/footer_main_dashboard.php
@@ -0,0 +1,26 @@
+      <!-- End Page Content -->
+      </div>
+      <!-- End Container fluid  -->
+      <!-- footer -->
+      <footer class="footer">Plataforma de Información Unificada - CI Estrada Velasquez. © <script>var d = new Date(); var n = d.getFullYear(); document.write(n);</script> Todos los derechos reservados.</footer>
+      <!-- End footer -->
+    </div>
+    <!-- End Page wrapper  -->
+  </div>
+  <!-- End Wrapper -->
+  <!-- All Jquery -->
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/lib/jquery/jquery.min.js'; ?>"></script>
+  <!-- Bootstrap tether Core JavaScript -->
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/lib/bootstrap/js/popper.min.js'; ?>"></script>
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/lib/bootstrap/js/bootstrap.min.js'; ?>"></script>
+  <!-- slimscrollbar scrollbar JavaScript -->
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/jquery.slimscroll.js'; ?>"></script>
+  <!--Menu sidebar -->
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/sidebarmenu.js'; ?>"></script>
+  <!--stickey kit -->
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/lib/sticky-kit-master/dist/sticky-kit.min.js'; ?>"></script>
+  <!--Custom JavaScript -->
+  <script src="<?php echo $this->config->item('assets_path').'themes/elaadmin/js/custom.min.js'; ?>"></script>
+</body>
+
+</html>

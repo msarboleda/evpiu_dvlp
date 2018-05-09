bdc080ffc3c022d05c1c06ee3ade740910821563
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-05-09 14:31:45 -0500

    Actualización a modelo :: ModulosxCategoriasxGrupos
    - Se añade un alias a la tabla principal para reducir un poco el tamaño del código.
    - Se añade el campo de Icono para mostrarse dinámicamente en la barra de navegación.

--- webroot/application/models/EVPIU/ModulosxCategoriasxGrupos_model.php
+++ webroot/application/models/EVPIU/ModulosxCategoriasxGrupos_model.php
@@ -12,7 +12,7 @@ class ModulosxCategoriasxGrupos_model extends CI_Model {
 	public function __construct() {
 		parent::__construct();
 
-		$this->_table = 'V_ModulosxCategoriasxGrupos';
+		$this->_table = 'V_ModulosxCategoriasxGrupos V';
 		$this->db_evpiu = $this->load->database('EVPIU', true);
 	}
 
@@ -23,10 +23,11 @@ class ModulosxCategoriasxGrupos_model extends CI_Model {
 	 * @return false En caso de que el Query no tenga ningún resultado
 	 */
 	public function consultarCantModulos_x_Categorias_x_Grupos($grupos) {
-		$this->db_evpiu->select('CodCategoria, NomCategoria, COUNT(CodModulo) as Modulos');
+		$this->db_evpiu->select('V.CodCategoria, V.NomCategoria, COUNT(V.CodModulo) as Modulos, C.Icono');
 		$this->db_evpiu->from($this->_table);
   	$this->db_evpiu->where_in('Grupo', $grupos);
-    $this->db_evpiu->group_by('CodCategoria, NomCategoria');
+  	$this->db_evpiu->join('Categorias C', 'V.CodCategoria = C.CodCategoria', 'inner');
+    $this->db_evpiu->group_by('V.CodCategoria, V.NomCategoria, C.Icono');
     $query = $this->db_evpiu->get();
 
     if ($query->num_rows() > 0){

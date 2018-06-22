42fbbf870b371269f769a66de962a35707b1cc26
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-06-22 08:49:55 -0500

    feat: Controlador XHR de Requerimientos
    En este cambio se agrega un controlador para gestionar las peticiones Ajax que se van a manejar en el futuro controlador de Requerimientos.
    
    Consigo lleva una función que se encarga de cargar todos los clientes de un vendedor con sesión actual en la plataforma a un control <select>.

---
+++ webroot/application/modules/Requerimientos/controllers/XHR.php
@@ -0,0 +1,44 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Controlador: Requerimientos_XHR
+ *
+ * Este controlador se utiliza para gestionar las peticiones Ajax
+ * que se necesitan en el módulo de Requerimientos.
+ * 
+ * @author Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
+ * @copyright 2018 CI Estrada Velasquez y Cia S.A.S
+ */
+
+class XHR extends MX_Controller {
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->helper('language', 'load');
+		$this->load->model('Requerimientos/MAXEstrada/Customer_Master_model', 'Clientes_mdl');
+	}
+
+	/**
+	 * Genera a los clientes del vendedor actual, adaptados para que se muestren
+	 * en un Select con el plugin 'Select2'.
+	 *
+	 * Este método se encarga de consultar los clientes del vendedor actual y 
+	 * organizarlos en un formato adaptado para el plugin 'Select2'.
+	 *
+	 * @return string Múltiples clientes en etiquetas <option>.
+	 *		boolean En caso de que la consulta no arroje resultados.
+	 */
+	public function xhr_Customers_from_Current_Vendor_select() {
+		$vendor_code = $this->ion_auth->user()->row()->Vendedor;
+
+		$customers_select_data = $this->Clientes_mdl->fill_Clientes_from_Vendor_select($vendor_code);
+		
+		if (!empty($customers_select_data)) {
+			foreach ($customers_select_data as $key => $value) {
+				echo '<option value="'.$key.'">'.$value.'</option>';
+			}
+		}
+
+		return FALSE;
+	}
+}

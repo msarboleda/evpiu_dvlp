b8b07b0c30b6c6748751451776b1b58f0c2b2d23
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-06-27 11:29:33 -0500

    chore: Define la zona horaria a America/Bogota
    En este cambio se define la zona horaria -05:00 predeterminada para toda la aplicación.

--- webroot/application/config/config.php
+++ webroot/application/config/config.php
@@ -512,7 +512,7 @@ $config['compress_output'] = FALSE;
 | helper' page of the user guide for information regarding date handling.
 |
 */
-$config['time_reference'] = 'local';
+$config['time_reference'] = date_default_timezone_set('America/Bogota');
 
 /*
 |--------------------------------------------------------------------------

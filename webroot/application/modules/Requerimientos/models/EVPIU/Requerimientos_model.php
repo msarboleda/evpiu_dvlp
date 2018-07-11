588179fba9540af89c4e87e6ac9dd5a46175178e
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-07-11 14:17:11 -0500

    feat: Modelo de Requerimientos
    En este cambio se adjunta una función para agregar un requerimiento a la base de datos.

---
+++ webroot/application/modules/Requerimientos/models/EVPIU/Requerimientos_model.php
@@ -0,0 +1,49 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Modelo para Requerimientos
+ * 
+ * Este modelo se relaciona con la tabla de requerimientos;
+ * Tiene funciones dedicadas exclusivamente a la tabla definida
+ * dentro del constructor, principalmente se busca retornar todo
+ * tipo de dato relacionado con esta tabla.
+ *
+ * @author Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
+ * @copyright 2018 CI Estrada Velasquez y Cia S.A.S
+ */
+
+class Requerimientos_model extends CI_Model {
+	public function __construct() {
+		parent::__construct();
+
+		$this->_table = 'Requerimientos';
+		$this->db_evpiu = $this->load->database('EVPIU', true);
+	}
+
+	/**
+	 * Agrega un requerimiento a la base de datos.
+	 *
+	 * @param array $data Datos de un requerimiento para agregar a la base
+	 *        de datos.
+	 *
+	 * @return string Número de requerimiento que se almacenó.
+	 * 		boolean FALSE En caso de que los datos que se mandan como parámetro
+	 *        no tengan una estructura de array o el array de datos este vacío.
+	 */
+	public function add_Request($data = array()) {
+		if (is_array($data) && !empty($data)) {
+			$this->db_evpiu->insert($this->_table, $data);
+			$id = $this->db_evpiu->insert_id();
+
+			if (isset($id)) {
+				return $this->db_evpiu->select('NroRequerimiento')
+					->from($this->_table)
+					->where('idRequerimiento', $id)
+					->get()
+					->row('NroRequerimiento');
+			}
+		}
+
+		return FALSE;	
+	}
+}

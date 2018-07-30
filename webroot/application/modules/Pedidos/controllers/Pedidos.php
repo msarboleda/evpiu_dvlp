dd2d00e4318fe21b9d17b7eb58762837606e33e0
Martin Arboleda M <sistemas@estradavelasquez.com>
2018-07-30 16:50:53 -0500

    feat: Controlador Pedidos reforzado y vista de Listar pedidos nueva
    Refuerzo el controlador de Pedidos y creo la vista para listar los pedidos absado en la plantilla de Marcas.

--- webroot/application/modules/Pedidos/controllers/Pedidos.php
+++ webroot/application/modules/Pedidos/controllers/Pedidos.php
@@ -16,16 +16,35 @@
       $this->load->helper(array('language', 'load', 'form'));
     }
 
+    /**
+      * Metodo inicial del módulo de pedidos
+      */
     public function index() {
       if ($this->verification_roles->is_vendor() || $this->ion_auth->is_admin()) {
+        $this->add_Index_libraries();
+        
         $header_data = $this->header->show_Categories_and_Modules();
         $this->lang->load('index');
         $header_data['module_name'] = lang('index_heading');
 
         $this->load->view('headers'. DS .'header_main_dashboard', $header_data);
+        $this->load->view('pedidos'. DS .'index');
         $this->load->view('footers'. DS .'footer_main_dashboard');
       } else {
         redirect('auth');
       }
+    }
+    
+    /**
+      * @description Añade las librerias CSS y JS principales del método Index.
+      */
+    public function add_Index_libraries() {
+      $this->lang->load('index');
+
+      add_css('themes/elaadmin/css/lib/sweetalert2/sweetalert2.min.css');
+      add_js('themes/elaadmin/js/lib/datatables/datatables.min.js');
+      add_js('themes/elaadmin/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js');
+      add_js('themes/elaadmin/js/lib/sweetalert2/sweetalert2.min.js');
+      add_js('dist/custom/js/marcas/index.js');
     }
   }
---
+++ webroot/application/modules/Pedidos/views/index.php
@@ -0,0 +1,29 @@
+<div class="col-12">
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">Tabla de Pedidos</h4>
+      <div class="table-responsive m-t-40">
+        <table id="pedidos" class="display nowrap table table-hover table-striped table-bordered" cellspacing="0" width="100%">
+          <thead>
+            <tr>
+              <th scope="row">#</th>
+              <th>Número</th>
+              <th>Fecha</th>
+              <th class="text-left">Cliente</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th scope="row">#</th>
+              <th>Número</th>
+              <th>Fecha</th>
+              <th>Cliente</th>
+            </tr>
+          </tfoot>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>

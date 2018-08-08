178b363d01d9589f1080dbf79fa870a2b3fb96f8
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-08-08 12:59:20 -0500

    feat: Tabla de clientes para vendedores

---
+++ webroot/assets/dist/custom/js/reportes/clientes/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  const base_url = document.location.origin+'/';
+  const $my_clients_table = $('#my_clients');
+  
+  const my_clients = $my_clients_table.DataTable({
+	  ajax: {
+	    url: base_url+'xhr_clientes/xhr_getcustomersdatafromcurrentvendor',
+	    method: 'GET',
+	    dataSrc: function(response) {
+        if (response.exception) {
+          swal({
+            text: response.exception,
+            imageUrl: base_url+'assets/themes/elaadmin/emojipedia/pensive-face.png',
+          });
+        }
+
+        return response;
+      }
+    },
+    columns: [
+      { 
+        data: null,
+        render: function (data, type, full, meta) {
+          return meta.row + 1;
+        }
+      },
+      { data: 'CUSTID_23' },
+      { data: 'NAME_23' },
+      { data: 'UDFKEY_23' },
+      { 
+        data: null,
+        render: function (data) {
+          return data.ADDR1_23+' '+data.ADDR2_23;
+        }
+      },
+      { 
+        data: null,
+        render: function (data) {
+          return data.PHONE_23+' '+data.TELEX_23;
+        }
+      },
+      { 
+        data: 'STATUS_23',
+        render: function (data) {
+          if (data === 'R') {
+            return '<span class="badge badge-success">LIBERADO</span>';
+          }
+
+          return '<span class="badge badge-danger">RETENIDO</span>';
+        }
+      },
+    ],
+	  language: {
+	    url: base_url+'assets/themes/elaadmin/js/lib/datatables/spanish.json'
+	  },
+	  dom: 'B<"row">lfrtip',
+	  buttons: [
+      {
+        text: '<i class="fa fa-refresh"></i> Actualizar',
+        action: function ( e, dt, node, config ) {
+          dt.ajax.reload();
+        }
+      }
+    ]
+	});    
+});

bcea8c2e7b110c71a2faf6d5da42270398bf7f53
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-08-08 10:34:08 -0500

    feat: Estructura base de reporte de clientes

--- webroot/application/modules/Reportes/controllers/Clientes.php
+++ webroot/application/modules/Reportes/controllers/Clientes.php
@@ -13,12 +13,42 @@
 class Clientes extends MX_Controller {
   public function __construct() {
     parent::__construct();
+
+    $this->load->model('Auth/EVPIU/ModulosxCategoriasxGrupos_model');
+    $this->load->library(array('header', 'verification_roles'));
+    $this->load->helper(array('language', 'load', 'form'));
   }
 
   /**
-   * Página principal de Reporte de Clientes
+   * @description Página principal de Reporte de Clientes
    */
   public function index() {
-    echo 'Hello world';
+    if ($this->verification_roles->is_vendor() || $this->ion_auth->is_admin()) {
+      $header_data = $this->header->show_Categories_and_Modules();
+      $this->add_Index_libraries();
+      $header_data['module_name'] = lang('index_heading');
+
+      $this->load->view('headers'. DS .'header_main_dashboard', $header_data);
+      $this->load->view('reportes/clientes'. DS .'index');
+      $this->load->view('footers'. DS .'footer_main_dashboard');
+    } else {
+      redirect('auth');
+    }
+  }
+
+  /**
+   * @description Añade las librerias CSS y JS principales del
+   * método Index.
+   * 
+   * @return void 
+   */
+  public function add_Index_libraries() {
+    $this->lang->load('clientes/index');
+
+    add_css('themes/elaadmin/css/lib/sweetalert2/sweetalert2.min.css');
+    add_js('themes/elaadmin/js/lib/datatables/datatables.min.js');
+    add_js('themes/elaadmin/js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js');
+    add_js('themes/elaadmin/js/lib/sweetalert2/sweetalert2.min.js');
+    add_js('dist/custom/js/reportes/clientes/index.js');
   }
 }
---
+++ webroot/application/modules/Reportes/language/spanish/clientes/index_lang.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* Archivo de lenguajes en español para el Reporte de Clientes.
+*
+* @author Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
+* @copyright 2018 CI Estrada Velasquez y Cia S.A.S
+*/
+
+// Page title
+$lang['index_heading'] = 'Clientes';

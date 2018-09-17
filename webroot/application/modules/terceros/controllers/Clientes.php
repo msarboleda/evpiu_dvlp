ca6fa57c7356b5ba7b6e2fffba28a5b96863dbd3
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-09-17 08:40:20 -0500

    feat: Busca el vendedor asignado a un cliente

---
+++ webroot/application/modules/terceros/controllers/Clientes.php
@@ -0,0 +1,36 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Clase de Clientes
+ *
+ * Descripción del controlador.
+ * 
+ * @author Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
+ * @author Martin Arboleda Montoya <maarboleda@estradavelasquez.com>
+ * @copyright 2018 CI Estrada Velasquez y Cia S.A.S
+ */
+
+class Clientes extends MX_Controller {
+  public function __construct() {
+    parent::__construct();
+
+    $this->load->model('Terceros/estradav/Clientes_dms_model', 'Clientes_dms_mdl');
+  }
+
+  /**
+   * Busca el vendedor asignado de un cliente de DMS.
+   * 
+   * @param int $nit NIT del Cliente.
+   * 
+   * @return object
+   */
+  public function find_vendor_assigned_to_dms_customer($nit) {
+    try {
+      return $this->Clientes_dms_mdl->find_vendor_assigned_to_customer($nit);
+    } catch (InvalidArgumentException $e) {
+      return 'Argumento inválido: ' . $e->getMessage();
+    } catch (Exception $e) {
+      return 'Error: ' . $e->getMessage();
+    }
+  }
+}
---
+++ webroot/application/modules/terceros/models/estradav/Clientes_dms_model.php
@@ -0,0 +1,42 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Modelo: Clientes DMS
+ * 
+ * Descripción del modelo
+ *
+ * @author Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
+ * @copyright 2018 CI Estrada Velasquez y Cia S.A.S
+ */
+
+class Clientes_dms_model extends CI_Model {
+	public function __construct() {
+    parent::__construct();
+    
+		$this->db_dms = $this->load->database('DMS', true);
+  }
+  
+  /**
+   * Busca el vendedor asignado de un cliente de DMS.
+   * 
+   * @param int $nit NIT del Cliente.
+   * 
+   * @return object
+   */
+  public function find_vendor_assigned_to_customer($nit) {
+    if (!isset($nit) || empty($nit)) {
+      throw new \InvalidArgumentException('El parámetro de nit está vacío.');
+    }
+
+    $this->db_dms->select('vendedor, NombreVendedor');
+    $this->db_dms->where('nit', $nit);
+
+    $query = $this->db_dms->get('V_CIEV_Clientes');
+
+    if ($query->num_rows() > 0) {
+      return $query->row();
+    } else {
+      throw new Exception('No se obtuvo ningun resultado del cliente solicitado.');
+    }
+  }
+}

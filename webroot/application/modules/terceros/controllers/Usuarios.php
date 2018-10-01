07d359341749da52e953eb9ca317def1c6fd9e7d
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-10-01 07:17:27 -0500

    feat: Formateo con .editorconfig

--- webroot/application/modules/terceros/controllers/Usuarios.php
+++ webroot/application/modules/terceros/controllers/Usuarios.php
@@ -4,7 +4,7 @@
  * Clase de Usuarios
  *
  * Descripción de la clase
- * 
+ *
  * @author Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
  * @copyright 2018 CI Estrada Velasquez y Cia S.A.S
  */
@@ -12,14 +12,16 @@
 class Usuarios extends MX_Controller {
   public function __construct() {
     parent::__construct();
-    
+
     $this->load->model('Terceros/users/Usuarios_model', 'Usuarios_mdl');
   }
 
   /**
    * Poblar un control <select> con todos los usuarios 
+  /**
+   * Poblar un control <select> con todos los usuarios
    * existentes de la plataforma.
-   * 
+   *
    * @return object
    */
   public function populate_users() {

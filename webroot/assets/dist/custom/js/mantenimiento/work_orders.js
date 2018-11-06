95bb1146f7ea21ec53ed7be4d0f8b3b7b53e4948
Santiago Arboleda Londoño <msarboleda@estradavelasquez.com>
2018-11-06 14:15:19 -0500

    feat: Consulta todos los tipos de trabajos de mantenimiento

--- webroot/assets/dist/custom/js/mantenimiento/work_orders.js
+++ webroot/assets/dist/custom/js/mantenimiento/work_orders.js
@@ -19,3 +19,25 @@ function get_all_maintenance_types() {
 
   return maintenanceTypesOptionsPromise;
 }
+
+/**
+ * Obtiene todos los tipos de trabajo existentes.
+ *
+ */
+function get_all_work_types() {
+  let app_url = document.location.origin + '/';
+
+  var maintenanceWorkOptionsPromise = new Promise(function (resolve, reject) {
+    $.getJSON(app_url+'mantenimiento/ordenes_trabajo/xhr_get_all_work_types').done(function(response) {
+      if (response.content) {
+        reject(response);
+      } else {
+        resolve(response);
+      }
+    }).fail(function() {
+      reject(new Error('No se puede localizar el recurso para cargar los tipos de trabajo.'));
+    });
+  });
+
+  return maintenanceWorkOptionsPromise;
+}
